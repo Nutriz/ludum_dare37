b70ac1259481d2ad0f4b41eb8d70aa2005fcc9f7
Jerome67000 <jerome.gully0@gmail.com>
2016-12-21 20:40:56 +0100

    fix export exe on windows

--- scripts/World.gd
+++ scripts/World.gd
@@ -6,7 +6,7 @@ func _ready():
 func _input(event):
 	# Quit the game:
 	if Input.is_action_pressed("quit"):
-		var menuScene = preload("res://scenes/Menu.tscn")
+		var menuScene = preload("res://scenes/menu.tscn")
 		var menu = menuScene.instance()
 		get_tree().get_root().add_child(menu)
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
--- scripts/menu.gd
+++ scripts/menu.gd
@@ -7,7 +7,7 @@ func _on_ButtonQuit_pressed():
 	get_tree().quit()
 
 func _on_ButtonPlay_pressed():
-	var worldScene = preload("res://Scenes/World.tscn")
+	var worldScene = preload("res://scenes/world.tscn")
 	var world = worldScene.instance()
 	get_tree().get_root().add_child(world)
 	queue_free()
